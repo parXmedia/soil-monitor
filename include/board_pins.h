c2111f62c2b748e7512ba90a41b3fc52dd8a5e4e
parXmedia <v9b45dw27r@privaterelay.appleid.com>
2026-08-09 13:14:22 -0600

    Add include/board_pins.h

---
+++ include/board_pins.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// LAFVIN ESP32-S3 1.69-inch LCD board (ST7789, 240x280).
+constexpr int LCD_MOSI = 39;
+constexpr int LCD_SCK = 38;
+constexpr int LCD_CS = 14;
+constexpr int LCD_DC = 10;
+constexpr int LCD_RST = 3;
+constexpr int LCD_BL = 13;
+constexpr int LCD_WIDTH = 240;
+constexpr int LCD_HEIGHT = 280;
+
+constexpr int BOOT_BUTTON = 0;
+
